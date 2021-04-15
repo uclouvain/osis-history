b9792c8b044a9e174a5829efd586736602cbf9b9
jmr <jmr@makina-corpus.com>
2021-04-15 16:48:19 +0200

    Test post_delete signal with history_test app creation

---
+++ tests/history_test/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HistoryTestConfig(AppConfig):
+    name = "history.tests.history_test"
---
+++ tests/signals/test_receivers.py
@@ -0,0 +1,21 @@
+import uuid
+
+from django.test import TestCase
+
+from history.models import HistoryEntry
+from history.tests.history_test.models import DummyModel
+
+
+class AdmissionDoctorateCreateViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.dumb_instance = DummyModel.objects.create(uuid=uuid.uuid4())
+        cls.history_entry = HistoryEntry.objects.create(
+            uuid=cls.dumb_instance.uuid, message="a test message"
+        )
+
+    def test_delete_instance_must_delete_related_history_entries(self):
+        all_history_entries = HistoryEntry.objects.all()
+        self.assertEqual(all_history_entries.count(), 1)
+        self.dumb_instance.delete()
+        self.assertEqual(all_history_entries.count(), 0)
