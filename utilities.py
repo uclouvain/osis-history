8ffc8e73411cac42cc29bfeabd23fc18e008148c
jmr <jmr@makina-corpus.com>
2021-04-16 17:29:40 +0200

    Rework typing

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
-import uuid
+from uuid import UUID
 from datetime import datetime
-from typing import Tuple
+from typing import List, Tuple, Union
 
 from history.models import HistoryEntry
 
@@ -10,7 +10,7 @@ __all__ = [
 ]
 
 
-def add_history_entry(object_uuid: uuid, message: str) -> None:
+def add_history_entry(object_uuid: Union[str, UUID], message: str) -> None:
     """Add a HistoryEntry related to the given object's uuid with a custom message.
 
     :param object_uuid: The object's instance uuid
@@ -19,13 +19,13 @@ def add_history_entry(object_uuid: uuid, message: str) -> None:
     HistoryEntry.objects.create(object_uuid=object_uuid, message=message)
 
 
-def get_history_entries(object_uuid: uuid) -> Tuple[datetime, str]:
+def get_history_entries(object_uuid: Union[str, UUID]) -> List[Tuple[datetime, str]]:
     """Return all the history entries related to a given objet's uuid.
 
     :param object_uuid: The object's instance uuid
     :return: A tuple of datetime and string that each represent a history entry
     """
-    return tuple(
+    return list(
         HistoryEntry.objects.filter(
             object_uuid=object_uuid
         ).values_list(
