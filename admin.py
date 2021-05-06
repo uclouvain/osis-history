76bd76ac5d68383ca929505687f70688d96a8c46
Sebastien Corbin <seb.corbin@gmail.com>
2021-05-06 11:09:28 +0200

    Better admin

--- admin.py
+++ admin.py
@@ -3,7 +3,13 @@ from osis_history.models import HistoryEntry
 
 
 class HistoryEntryAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'object_uuid',
+        'created',
+        'author',
+    )
+    search_fields = ('object_uuid', 'author')
+    date_hierarchy = 'created'
 
 
 admin.site.register(HistoryEntry, HistoryEntryAdmin)
